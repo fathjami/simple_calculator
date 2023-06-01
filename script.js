505a57e3dd90f3045b311554bc4febce6361d39b
Fathiya Jamia <fathiyajamaa051@gmail.com>
2023-06-01 18:35:30 +0100

    execute some operation, still needs some fixes

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
-let numbers = document.querySelectorAll(".number");
 
-numbers.forEach((number) => {
-
-    number.addEventListener("click", () => {
-        let screen = document.querySelector(".screen");
+const calculator = document.querySelector(".calculator");
+let result = 0;
+let op = "";
+calculator.addEventListener("click", (event) => {
+    const screen = document.querySelector(".screen");
+    if (event.target.classList.contains("number")) {
         if (screen.textContent === "0")
             screen.textContent = "";
-        screen.textContent += number.textContent;
-        console.log(number.textContent);
-    });
-});
-
-let deleteButton = document.querySelector(".delete");
-deleteButton.addEventListener("click", () => {
-    let screen = document.querySelector(".screen");
-    screen.textContent = screen.textContent.slice(0, -1);
-    if (screen.textContent === "")
+        screen.textContent += event.target.textContent;
+    }
+    if (event.target.classList.contains("delete")) {
+        screen.textContent = screen.textContent.slice(0, -1);
+        if (screen.textContent === "")
+            screen.textContent = "0";
+    }
+    if (event.target.classList.contains("clear")) {
         screen.textContent = "0";
+    }
+    if (event.target.classList.contains("operator") && event.target.textContent !== "=") {
+     const number = Number(screen.textContent);
+        const operator = event.target.textContent;
+        screen.textContent = "0";
+        if (operator === "+") {
+            result += number;
+        }
+        if (operator === "-") {
+            result -= number;
+        }
+        if (operator === "x") {
+            result *= number;
+        }
+        if (operator === "/") {
+            result /= number;
+        }
+        op = operator;
+    }
+
+    if (event.target.classList.contains("equal")) {
+        if (op === "+") {
+            screen.textContent = result + Number(screen.textContent);
+        }
+        if (op === "-") {
+            screen.textContent = result + Number(screen.textContent);
+        }
+        if (op === "x") {
+            screen.textContent = result * Number(screen.textContent);
+        }
+        if (op === "/") {
+            screen.textContent = result / Number(screen.textContent);
+        }
+        result = 0;
+    }
 });
